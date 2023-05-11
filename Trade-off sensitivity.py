d1a190ad3d377fcafd7492e5eb25c07390673e59
tleniger <tleniger@student.tudelft.nl>
2023-05-11 10:17:58 +0200

    modified:   Trade-off sensitivity.py, code now automatically takes the scores only modified:   Trade Off.xlsx corrected trade off excel

--- Trade-off sensitivity.py
+++ Trade-off sensitivity.py
@@ -4,3 +4,9 @@ import matplotlib.pyplot as plt
 
 data = pd.read_excel("Trade Off.xlsx", sheet_name="General Trade-off table", usecols=(np.arange(0,12,1)), nrows=17)
 
+scores = data.head(15)
+columns = np.array(scores.columns)
+for i in range(2, (len(columns))):
+    scores[columns[i]] = scores[columns[i]].str.split(";").str[0]
+
+
