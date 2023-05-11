87a2c74d18b8767fc97d405a605a7cbcf5bcd1e6
taalbers <taalbers@student.tudelft.nl>
2023-05-11 14:11:49 +0200

    Working 1st version of class-I estimation

--- Class-I-weight estimation.py
+++ Class-I-weight estimation.py
@@ -5,8 +5,11 @@ import parameters as para
 
 class Weight:
     def __init__(self, para):
+        # parameters.py is a file containing all constant parameters of the aircraft
         self.para = para
+        # take_off weight that is assumed initially
         self.W_TO = 750
+
     #########################################################################
     "CLASS I WEIGHT ESTIMATION"
     #########################################################################
@@ -14,7 +17,7 @@ class Weight:
     def weight_take_off(self):
 
         W_TO = self.W_OE + self.W_F + self.W_PL
-        return(W_TO)
+        return W_TO
 
 
     def weight_empty_operational(self):
@@ -38,16 +41,16 @@ class Weight:
         W7W5 = self.para.W4W3 * self.para.W10W9
         return(W7W5)
 
-    def Mff(self):
+    def Mff_calculation(self):
         self.R = self.para.R/self.para.n_drops
         W7W5 = weight.W7W5()
-        print(W7W5)
+
         W5W4 = weight.W5W4()
-        print(W5W4)
+
         self.Mff = self.para.W1W_TO * self.para.W2W1 * self.para.W3W2 * self.para.W4W3 * W5W4 * W7W5 * W5W4 * self.para.W10W9 * self.para.WfinalW10
 
     def weight_fuel_used(self):
-        weight.Mff()
+        weight.Mff_calculation()
 
         self.M_f_used = (1 - self.Mff) * self.W_TO
 
@@ -63,7 +66,6 @@ class Weight:
             weight.weight_empty_operational()
             self.W_PL = self.para.W_PL
             W_TO_new = weight.weight_take_off()
-            print(W_TO_new)
             change = (W_TO_new - self.W_TO)/self.W_TO
             if change < 0.01:
                 it = False
