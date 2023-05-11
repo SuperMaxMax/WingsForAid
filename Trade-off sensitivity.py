36aa647bbf3c4fd7818a7b3307f6d1ad22f7d60f
Jarno11 <jarno.platenburg@gmail.com>
2023-05-11 11:06:50 +0200

    a

--- Trade-off sensitivity.py
+++ Trade-off sensitivity.py
@@ -9,6 +9,8 @@ columns = np.array(scores.columns)
 for i in range(2, (len(columns))):
     scores[columns[i]] = scores[columns[i]].str.split(";").str[0]
 
+print(scores)
+
 def model(weight_vec, scores_mat):
     weight_vec = np.array(weight_vec)
     scores_mat = np.array(scores_mat) 
@@ -16,5 +18,6 @@ def model(weight_vec, scores_mat):
     output = np.matmul(scores_mat, weight_vec)
     output = np.argsort(output)
     return output
+
 
 
