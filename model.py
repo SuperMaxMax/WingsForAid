fa82ad4a42ac30f23fa9a480f78f9d195adf7592
Jarno11 <jarno.platenburg@gmail.com>
2023-05-11 11:45:44 +0200

    Update model.py

--- model.py
+++ model.py
@@ -7,4 +7,9 @@ def model(weight_vec, scores_mat):
     output = np.matmul(scores_mat, weight_vec)
     return output
 
+import pandas as pd
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+data = pd.read_excel("Trade Off.xlsx", sheet_name="General Trade-off table", usecols=(np.arange(0,12,1)), nrows=17)
 
+print(4)
