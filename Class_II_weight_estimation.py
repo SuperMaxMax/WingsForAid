11be92ff28bbe55c98a054a60b67b1e738994a92
Jarno11 <jarno.platenburg@gmail.com>
2023-05-12 14:06:25 +0200

    a

--- Class_II_weight_estimation.py
+++ Class_II_weight_estimation.py
@@ -82,7 +82,6 @@ class Weight:
     def weight_empty(self):
         self.W_OEW = self.W_pg + self.W_sc + self.W_f + self.W_eq + self.W_n + self.W_uc + self.W_t + self.W_w
 
-        print((f"W_pg:{self.W_pg}"))
         print(f"W_OEW:{self.W_OEW}")
         return self.W_OEW
 
