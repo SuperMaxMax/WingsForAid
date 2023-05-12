de22b4684a32c759e6d0a720e70df21a0fe2e5b3
Jarno11 <jarno.platenburg@gmail.com>
2023-05-12 15:15:54 +0200

    fix

--- Class_II_weight_estimation.py
+++ Class_II_weight_estimation.py
@@ -80,13 +80,11 @@ class Weight:
     def weight_empty(self):
         self.W_OEW = self.W_pg + self.W_sc + self.W_f + self.W_eq + self.W_n + self.W_uc + self.W_t + self.W_w
 
-    def weight_take_off(self):
-        self.W_TO = self.W_pg + self.W_sc + self.W_f + self.W_eq + self.W_n + self.W_t + self.W_w # + self.W_uc
+    def weight_empty(self):
+        self.W_OEW = self.W_pg + self.W_sc + self.W_f + self.W_eq + self.W_n + self.W_t + self.W_w  + self.W_uc
         print((f"W_pg:{self.W_pg}"))
-        print(f"W_TO:{self.W_TO}")
-        return self.W_TO
-
         print(f"W_OEW:{self.W_OEW}")
+
         return self.W_OEW
 
     def cg_calc(self):
