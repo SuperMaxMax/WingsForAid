feb994601f79302128ef239496d70dfb1d2f92eb
taalbers <taalbers@student.tudelft.nl>
2023-05-12 11:04:37 +0200

    Worked further on the class-II estimation

--- Class_II_weight_estimation.py
+++ Class_II_weight_estimation.py
@@ -20,17 +20,15 @@ class Weight:
         b_ref = 1.905
         t_r = self.para.t_c * self.para.cwr
 
-        W_w = k_w * b_s**0.75 * (1 + (b_ref/self.para.b_s)**0.5) * self.para.n_ult**0.55 * ((b_s/t_r)/self.para.W_loading)**0.3 * self.para.W_G
+        self.W_w = k_w * b_s**0.75 * (1 + (b_ref/self.para.b_s)**0.5) * self.para.n_ult**0.55 * ((b_s/t_r)/self.para.W_loading)**0.3 * self.para.W_G
         
         #ADD 30% IF BRACED WING USED, 10% IF STRUT USED?
-        
-        return W_w
+
     
     def tail_weight(self): #ultimate load factor, tail surface area
-        W_t = 0.64 * (self.para.n_ult * self.para.s_tail**2)**0.75
+        self.W_t = 0.64 * (self.para.n_ult * self.para.s_tail**2)**0.75
 
         #IF TAILPLANE AREA UNKNOWN, WEIGHT ASSUMED TO BE 3.5-4% OF EMPTY WEIGHT
-        return W_t
 
     def gear_weight(self):
         A1 = input("Give A for main gear (Table 8-6 Torenbeek)", )
@@ -47,45 +45,41 @@ class Weight:
 
         #Nose gear:
         W_uc2 = 1.08 * (A2 + B2 * self.para.W_TO**0.75 + C2 * self.para.W_TO + D2 * self.para.W_TO**1.5)
-
-        return W_uc1 + W_uc2
+        self.W_uc = W_uc1 + W_uc2
 
     def nacelle_weight(self): #Take off power in hp
-        W_n = 1.134 * self.para.P_TO**0.5
-        return W_n
+        self.W_n = 1.134 * self.para.P_TO**0.5
 
 
     def equipment_weight(self):
-        return 0.08 * self.para.W_TO #MORE DETAILED ESTIMATION CAN BE MADE BUT NOT NECESSARY FOR TRADE-OFF
+        self.W_eq = 0.08 * self.para.W_TO
+        #MORE DETAILED ESTIMATION CAN BE MADE BUT NOT NECESSARY FOR TRADE-OFF
 
     def fuselage_weight(self):
         k_wf = 0.23
-        W_f = self.para.k_wf*(self.para.V_D*(self.para.l_t/(self.para.b_f+self.para.h_f)))**0.5*self.para.S_G**1.2
+        self.W_f = self.para.k_wf*(self.para.V_D*(self.para.l_t/(self.para.b_f+self.para.h_f)))**0.5*self.para.S_G**1.2
 
         # Add 7% if the main landing gear is attached to the fuselage, but 4% can be subtracted from the fuselage weight if there is no attachment structure for the main landing gear
         # Add 10% for freighter aircraft
         # For booms l_t is defined as distance between local wing chord and horizontal tailplane
 
-        return W_f
-
     def control_surface_weight(self):
         k_sc = 0.44*0.768
-        W_sc = k_sc*self.para.W_TO**(2/3)
+        self.W_sc = k_sc*self.para.W_TO**(2/3)
 
         # Add 20% for LE flap or slat
         # Add 15% for lift dumper controls
 
-        return W_sc
 
     def propulsion_weight(self):
         k_pg = 1.16 # tractor single propeller aircraft
-        W_pg = k_pg*self.para.N_e*(self.para.W_e+0.109*self.para.P_TO)
+        self.W_pg = k_pg*self.para.N_e*(self.para.W_e+0.109*self.para.P_TO)
 
         # If number of cylinder and volume of cylinder are known use figure 4-12 Torenbeek
 
-        return W_pg
 
-
+    def weight_take_off(self):
+        self.W_TO = self.W_pg + self.W_sc + self.W_f + self.W_eq + self.W_n + self.W_uc + self.W_t + self.W_w
 if __name__ == "__main__":
     weight = Weight(para)
 
