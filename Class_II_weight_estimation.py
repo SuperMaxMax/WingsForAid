6bf98039d690ed2c0413168a24f242b6e677511c
Timdnb <timdnblanken@gmail.com>
2023-05-15 13:29:51 +0200

    update class I and II

--- Class_II_weight_estimation.py
+++ Class_II_weight_estimation.py
@@ -1,29 +1,27 @@
-import numpy as np
-from parameters import *
-
+from math import cos
 
 #########################################################################
 "CLASS II WEIGHT ESTIMATION"
 #########################################################################
 
-def wing_weight(W_TO):   #Span, sweep, ultimate load factor, thickness over chord, cord length at root, wing loading, gross weight
+def wing_weight(obj):   #Span, sweep, ultimate load factor, thickness over chord, cord length at root, wing loading, gross weight
     k_w = 4.9E-3
-    b_s = b / np.cos(lambda_mid)
+    b_s = obj.b / cos(obj.lambda_mid)
     b_ref = 1.905
-    t_r = t_c * cwr
+    t_r = obj.t_c * obj.cwr
 
-    W_w = k_w * b_s**0.75 * (1 + (b_ref/b_s)**0.5) * n_ult**0.55 * ((b_s/t_r)/W_loading)**0.3 * W_TO
+    W_w = k_w * b_s**0.75 * (1 + (b_ref/b_s)**0.5) * obj.n_ult**0.55 * ((b_s/t_r)/(obj.W_TO/obj.S))**0.3 * obj.W_TO
 
     #ADD 30% IF BRACED WING USED, 10% IF STRUT USED?
     return W_w
 
-def tail_weight(): #ultimate load factor, tail surface area
-    W_t = 0.64 * (n_ult * s_tail**2)**0.75
-    return W_t
+def tail_weight(obj): #ultimate load factor, tail surface area
+    W_t = 0.64 * (obj.n_ult * obj.s_tail**2)**0.75
+    
     #IF TAILPLANE AREA UNKNOWN, WEIGHT ASSUMED TO BE 3.5-4% OF EMPTY WEIGHT
+    return W_t
 
-def gear_weight(W_TO):
-
+def gear_weight(obj):
     maingear_type = input("Is the main gear fixed or retractable [f/r]",)
     nosegear_type = input("Is the nose gear fixed or retractable [f/r]",)
 
@@ -54,88 +52,91 @@ def gear_weight(W_TO):
         D2 = 2.97 * 10**-6
 
     #Main gear:
-    W_uc1 = 1.08 * (A1 + B1 * W_TO**0.75 + C1 * W_TO + D1 * W_TO**1.5)
+    W_uc1 = 1.08 * (A1 + B1 * obj.W_TO**0.75 + C1 * obj.W_TO + D1 * obj.W_TO**1.5)
 
     #Nose gear:
-    W_uc2 = 1.08 * (A2 + B2 * W_TO**0.75 + C2 * W_TO + D2 * W_TO**1.5)
+    W_uc2 = 1.08 * (A2 + B2 * obj.W_TO**0.75 + C2 * obj.W_TO + D2 * obj.W_TO**1.5)
     W_uc = W_uc1 + W_uc2
+
     return W_uc
 
-def nacelle_weight(): #Take off power in hp
-    W_n = 1.134 * P_TO**0.5
+def nacelle_weight(obj): #Take off power in hp
+    W_n = 1.134 * obj.P_TO**0.5
+
     return W_n
 
-def equipment_weight(W_TO):
-    W_eq = 0.008 * W_TO
-    return W_eq
-    #MORE DETAILED ESTIMATION CAN BE MADE BUT NOT NECESSARY FOR TRADE-OFF
+def equipment_weight(obj):
+    W_eq = 0.008 * obj.W_TO
 
-def fuselage_weight():
+    #MORE DETAILED ESTIMATION CAN BE MADE BUT NOT NECESSARY FOR TRADE-OFF
+    return W_eq
+
+def fuselage_weight(obj):
     k_wf = 0.23
-    W_fus = k_wf*(V_D*(l_t/(b_f+h_f)))**0.5*S_G**1.2
-    return W_fus
+    W_fus = k_wf*(obj.V_D*(obj.l_t/(obj.b_f+obj.h_f)))**0.5*obj.S_G**1.2
+
     # Add 7% if the main landing gear is attached to the fuselage, but 4% can be subtracted from the fuselage weight if there is no attachment structure for the main landing gear
     # Add 10% for freighter aircraft
     # For booms l_t is defined as distance between local wing chord and horizontal tailplane
+    return W_fus
 
-def control_surface_weight(W_TO):
+
+def control_surface_weight(obj):
     k_sc = 0.44*0.768
-    W_sc = k_sc*W_TO**(2/3)
-    return W_sc
+    W_sc = k_sc*obj.W_TO**(2/3)
+
     # Add 20% for LE flap or slat
     # Add 15% for lift dumper controls
+    return W_sc
 
-def propulsion_weight():
+def propulsion_weight(obj):
     k_pg = 1.16 # tractor single propeller aircraft
-    W_pg = k_pg*N_e*(W_e+0.109*P_TO)
-    return W_pg
+    W_pg = k_pg*obj.N_e*(obj.W_e+0.109*obj.P_TO)
+
     # If number of cylinder and volume of cylinder are known use figure 4-12 Torenbeek
+    return W_pg
 
-def weight_empty(W_pg, W_sc, W_fus, W_eq, W_n, W_t, W_w, W_uc):
-    W_OEW = W_pg + W_sc + W_fus + W_eq + W_n + W_t + W_w  + W_uc
+def weight_empty(obj):
+    W_w = wing_weight(obj)
+    W_t = tail_weight(obj)
+    W_uc = gear_weight(obj)
+    W_n = nacelle_weight(obj)
+    W_eq = equipment_weight(obj)
+    W_fus = fuselage_weight(obj)
+    W_sc = control_surface_weight(obj)
+    W_pg = propulsion_weight(obj)
 
-    # Print all weights
-    print(f"W_pg:{round(W_pg,3)} [kg]")
-    print(f"W_sc:{round(W_sc,3)} [kg]")
-    print(f"W_fus:{round(W_fus)} [kg]")
-    print(f"W_eq:{round(W_eq)}")
-    print(f"W_n:{round(W_n)}")
-    print(f"W_t:{round(W_t)} [kg]")
-    print(f"W_w:{round(W_w)} [kg]")
-    print(f"W_uc:{round(W_uc)} [kg]")
+    obj.W_OE = W_pg + W_sc + W_fus + W_eq + W_n + W_t + W_w  + W_uc
 
-    # Operative empty weight
-    print(f"W_OEW:{W_OEW}")
+    # # Print all weights
+    # print(f"W_pg:{round(W_pg,3)} [kg]")
+    # print(f"W_sc:{round(W_sc,3)} [kg]")
+    # print(f"W_fus:{round(W_fus)} [kg]")
+    # print(f"W_eq:{round(W_eq)}")
+    # print(f"W_n:{round(W_n)}")
+    # print(f"W_t:{round(W_t)} [kg]")
+    # print(f"W_w:{round(W_w)} [kg]")
+    # print(f"W_uc:{round(W_uc)} [kg]")
 
-    return W_OEW
+    # # Operative empty weight
+    # print(f"W_OEW:{obj.W_OE}")
 
-def cg_calc():
-    wing_cg = 0
-    if sweep_angle == 0:
-        wing_cg = 0.4 * cwr + l_LE #40% of root chord plus Leading Edge location
-    else:
-        wing_cg = 0 # to be done later, depends on spar locations (table 8-15 Torenbeek)
+    # return obj.W_OE
 
-    fus_cg = 0.335 * l_f #32-35% of fuselage length
-    tail_cg = 0.42 * cwr + l_LE #42% of root chord plus Leading Edge location
+# def cg_calc():
+#     wing_cg = 0
+#     if sweep_angle == 0:
+#         wing_cg = 0.4 * cwr + l_LE #40% of root chord plus Leading Edge location
+#     else:
+#         wing_cg = 0 # to be done later, depends on spar locations (table 8-15 Torenbeek)
 
-    engine_cg = 0 # to be done later
-    # c.g. or rotax 912is is at 32.7 cm from the front of the engine
+#     fus_cg = 0.335 * l_f #32-35% of fuselage length
+#     tail_cg = 0.42 * cwr + l_LE #42% of root chord plus Leading Edge location
 
-    landing_gear_cg = 0 # to be done later
-    # can be at airplane c.g. -> iteration needed, or use location main and nose landing gear
+#     engine_cg = 0 # to be done later
+#     # c.g. or rotax 912is is at 32.7 cm from the front of the engine
 
-    cg = (wing_cg * W_w + fus_cg * W_f + tail_cg * W_t + engine_cg * W_pg + landing_gear_cg * W_uc) / (W_w + W_f + W_t + W_pg + W_uc)
+#     landing_gear_cg = 0 # to be done later
+#     # can be at airplane c.g. -> iteration needed, or use location main and nose landing gear
 
-if __name__ == "__main__":
-    W_TO = 600
-    W_w = wing_weight(W_TO)
-    W_t = tail_weight()
-    W_uc = gear_weight(W_TO)
-    W_n = nacelle_weight()
-    W_eq = equipment_weight(W_TO)
-    W_fus = fuselage_weight()
-    W_sc = control_surface_weight(W_TO)
-    W_pg = propulsion_weight()
-
-    weight_empty(W_pg, W_sc, W_fus, W_eq, W_n, W_t, W_w, W_uc)
+#     cg = (wing_cg * W_w + fus_cg * W_f + tail_cg * W_t + engine_cg * W_pg + landing_gear_cg * W_uc) / (W_w + W_f + W_t + W_pg + W_uc)
--- Class_I_weight_estimation.py
+++ Class_I_weight_estimation.py
@@ -1,7 +1,4 @@
-import numpy as np
-from math import *
-from parameters import *
-import matplotlib.pyplot as plt
+from math import sqrt, pi, exp
 
 #########################################################################
 "CLASS I WEIGHT ESTIMATION"
@@ -11,57 +8,57 @@ import matplotlib.pyplot as plt
 def weight_take_off(W_OE, W_F, W_PL):
     # take_off weight is a function of operational empty weight, fuel weight and payload weight
     W_TO = W_OE + W_F + W_PL
-    return W_TO
 
+    return W_TO
 
 def weight_empty_operational(obj):
     # Operational empty weigth is a function of the empty weight and the trapped fuel weight, which are both a function of take_off weight
     W_E = obj.lin_par1 * obj.W_TO + obj.lin_par2  # relation from Roskam
     W_tfo = 0.002 * obj.W_TO
     W_OE = W_E + W_tfo
-    return W_OE
 
+    return W_OE
 
 def L_D_cruise(obj):
     # Lift over drag calculation
-    L_D = np.sqrt(np.pi * obj.A * obj.e / (4 * obj.CD0))
-    return L_D
+    L_D = sqrt(pi * obj.A * obj.e / (4 * obj.CD0))
 
+    return L_D
 
 def W5W4_calculation(obj):
     # Cruise fuel fraction calculation
     L_D = L_D_cruise(obj) # Lift over drag ratio from method above
     W5W4 = 1/exp(obj.R * obj.g * obj.c_p / (obj.prop_eff * L_D)) #Breguet's range equation
-    return W5W4
 
+    return W5W4
 
 def W7W5_calculation(obj):
     # Dropping manoeuvre
     W7W5 = obj.W4W3 * obj.W10W9
-    return W7W5
 
+    return W7W5
 
 def Mff_calculation(obj):
     W7W5 = W7W5_calculation(obj)
     W5W4 = W5W4_calculation(obj)
 
     Mff = obj.W1W_TO * obj.W2W1 * obj.W3W2 * obj.W4W3 * W5W4 * W7W5**obj.n_drops * obj.W10W9 * obj.WfinalW10
-    return Mff
 
+    return Mff
 
 def weight_fuel_used(obj):
     Mff = Mff_calculation(obj)
 
     M_f_used = (1 - Mff) * obj.W_TO
-    return M_f_used
 
+    return M_f_used
 
 def weight_fuel(obj):
     M_f_used = weight_fuel_used(obj)
 
     W_F = (1 + obj.M_res) * M_f_used
-    return W_F
 
+    return W_F
 
 def iteration(obj):
     it = True
@@ -83,4 +80,3 @@ def iteration(obj):
     print(f"W_F:{obj.W_F}")
     print(f"W_PL:{obj.W_PL}")
     print(f"W_TO:{obj.W_TO}")
-
