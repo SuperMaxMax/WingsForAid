1bfa82c8df9953240f2fc17883d98785874c72a3
Timdnb <timdnblanken@gmail.com>
2023-05-15 14:45:06 +0200

    improve main

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 
 from parameters import UAV
-import Class_I_weight_estimation as C1
-import geometry_determination as geo
+import Class_I_weight_estimation as c1
+import Class_II_weight_estimation as c2
+# import geometry_determination as geo
 
-concept1 = UAV()
+concept_1 = UAV('concept_1')
+concept_2 = UAV('concept_2')
+concept_3 = UAV('concept_3')
+concept_4 = UAV('concept_4')
+concept_5 = UAV('concept_5')
 
-C1.iteration(concept1)
+# iteration
+it = True
 
-geo.wing_sizing(concept1, False)
+for concept in [concept_1, concept_2, concept_3, concept_4, concept_5]:
+    while it:
+        c1.iteration(concept)
+        W_OE_c1 = concept.W_OE
 
-print(concept1.L_D)
+        # geometry determination stuff
+
+        c2.weight_empty(concept)
+        W_OE_c2 = concept.W_OE
+    
+        change = (W_OE_c2 - W_OE_c1)/W_OE_c1
+        if abs(change) < 0.001:
+            it = False
+        else:
+            continue
+    
+    print(f"Concept {concept.name} has a weight of {concept.W_OE} kg")
