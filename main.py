e43a551e2556aa1a97e0dc1a7976b4b60c5d59cd
Timdnb <timdnblanken@gmail.com>
2023-05-17 15:01:51 +0200

    fix plotting issue

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ for concept in [CON_1, CON_1_braced, CON_2, CON_2_braced, CON_3, CON_3_braced, C
     # cg calculation
     plt.figure(1)
     plt.subplot(121)
-    c2cg.cg_calc(concept, plot=False)
+    c2cg.cg_calc(concept)
 
     # V-n diagram
     plt.subplot(122)
