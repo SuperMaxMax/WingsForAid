50e724785cb4e7122d99aab428843aa9d8b13ed1
Timdnb <timdnblanken@gmail.com>
2023-05-25 12:21:23 +0200

    update example files

--- operations/example.py
+++ operations/example.py
@@ -13,4 +13,10 @@ def print_name(obj):
     # print aircraft name
     print(obj.name)
 
+def overwrite_value(obj):
+    # overwrite aircraft name
+    obj.name = 'new name'
+
+print_name(aircraft)
+overwrite_value(aircraft)
 print_name(aircraft)
