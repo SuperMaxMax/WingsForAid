f46d100cf02b2104931c026f73262dd303025664
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2023-05-25 16:08:11 +0200

    modified:   flight_performance/simulation.py     altered working path of flight performance file

--- flight_performance/simulation.py
+++ flight_performance/simulation.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append("..")
+import os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 # Start your import below this
 # import packages
@@ -8,19 +9,6 @@ import scipy as sp
 # Import other files
 from parameters import UAV
 
-aircraft = UAV('aircraft')
+aircraft = UAV("aircraft")
 
-# print all attributes of aircraft
-# print(aircraft.__dict__)
-
-def print_name(obj):
-    # print aircraft name
-    print(obj.name)
-
-# def overwrite_value(obj):
-#     # overwrite aircraft name
-#     obj.name = 'new name'
-
-print_name(aircraft)
-# overwrite_value(aircraft)
-print_name(aircraft)
+print(aircraft.name)
