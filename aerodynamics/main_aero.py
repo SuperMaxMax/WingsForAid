d064efb6f648ec1dccf762dbbbb7b4c859392cfc
Jarno11 <jarno.platenburg@gmail.com>
2023-06-05 15:16:33 +0200

    main_aero

--- aerodynamics/main_aero.py
+++ aerodynamics/main_aero.py
@@ -1,6 +1,8 @@
 import sys
 import os.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from parameters import UAV
+
 
 import wing_planform as wp
 import horizontal_tail_design as htd
