aeb40309964ef7be923701b20e963bf549d290f6
thuegens <T.P.R.huegens@student.tudelft.nl>
2023-06-05 11:08:11 +0200

    Worked on exporting parameters.py

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ from parameters import UAV
 # # save dataframe to csv file
 # df.to_csv('concept_comparison.csv', sep=';')
 
-# if __name__ == __main__:
-#     AC = UAV('aircraft')
-#     print("AC default:", AC.__dict__)
-#
-#     # --- saving
-#     # save all attributes of object to csv file
-#     members = [attr for attr in dir(AC) if not callable(getattr(AC, attr)) and not attr.startswith("__")]
-#     values = [getattr(AC, member) for member in members]
-#
-#     # remove brackets and round values
-#     values = [value[0] if isinstance(value, np.ndarray) else value for value in values]
-#     values = [round(value, 4) if isinstance(value, float) else value for value in values]
-#
-#     # add to dataframe
-#     df[AC.name] = values
+if __name__ == '__main__':
+    AC = UAV('aircraft')
+    print("AC default:", AC.__dict__)
+
+    # --- saving
+    # save all attributes of object to csv file
+    members = [attr for attr in dir(AC) if not callable(getattr(AC, attr)) and not attr.startswith("__")]
+    values = [getattr(AC, member) for member in members]
+
+    # remove brackets and round values
+    values = [value[0] if isinstance(value, np.ndarray) else value for value in values]
+    values = [round(value, 4) if isinstance(value, float) else value for value in values]
+
+    # add to dataframe
+    df[AC.name] = values
+    df.to_csv('output.csv', sep=';')
