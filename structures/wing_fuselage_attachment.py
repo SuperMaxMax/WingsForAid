df464491318dbc35f2555e555bbe43932af018bb
jwiacek <jwiacek@student.tudelft.nl>
2023-06-09 17:57:34 +0200

    wing attachment calculations

--- structures/wing_fuselage_attachment.py
+++ structures/wing_fuselage_attachment.py
@@ -31,8 +31,29 @@ c = R_pin
 rho_s = R_pin
 Ksc = 1 + alpha*(c/rho_s)**0.5
 R_notch_fus = 0.025
+R_notch_w = 0.05
 
 A_notch = abs(Ay_min)*Ksc/sigma_yield_shear
 
 t_notch_fus = abs(Ay_min)*Ksc*0.5/(sigma_yield_shear*(R_notch_fus-R_pin))
-print('thickness notsh fuselage',t_notch_fus)
+print('thickness notsh fuselage for shear out mm',t_notch_fus*1000)
+t_notch_w = abs(Ay_min)*Ksc/(sigma_yield_shear*(R_notch_w-R_pin))
+print('thickness notch wing for shear out mm',t_notch_w*1000)
+
+#net section failure
+
+t_notch_fus_1 = abs(Ay_min)*Ksc*0.5/(2*sigma_yield*(R_notch_fus-R_pin))
+t_notch_w_1 = abs(Ay_min)*Ksc/(2*sigma_yield*(R_notch_w-R_pin))
+print('thickness notsh fuselage mm for section failure',t_notch_fus_1*1000)
+print('thickness notch wing for section failure mm',t_notch_w_1*1000)
+
+# compression failure yield
+
+t_notch_fus_2 = abs(Ay_max)*Ksc*0.5/(2*R_pin*sigma_yield)
+t_notch_w_2 = abs(Ay_max)*Ksc/(2*R_pin*sigma_yield)
+
+t_notch_fus = max(t_notch_fus_1,t_notch_fus,t_notch_fus_2)
+t_notch_w = max(t_notch_w,t_notch_w_1,t_notch_w_2)
+
+print('t notch fus final mm',t_notch_fus*1000)
+print('t notch wing final mm',t_notch_w*1000)
