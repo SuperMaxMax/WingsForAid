3aba3066339e283fa477182b7d423ebbedfab8a6
Jan Vonken <j.vonken@student.tudelft.nl>
2023-06-12 21:05:23 +0200

    fix in spaces in geomtry file

--- aerodynamics/avl.py
+++ aerodynamics/avl.py
@@ -20,6 +20,12 @@ wp.main_wing_planform(aircraft)
 htd.horizontal_tail_planform(aircraft)
 vtd.horizontal_tail_planform(aircraft)
 
+df = pd.read_csv("AVL_mass.csv")
+masses = np.savetxt("AVL_mass.txt", df, delimiter=" ", fmt='%s')
+
+f = open('AVL_mass.txt', 'r')
+file_contents = f.read()
+
 avl_mass = """#-------------------------------------------------
 #  Wings For Aid Aircraft
 #
@@ -49,17 +55,11 @@ rho = 0.904637
 #     (same orientation, same origin location, same length units)
 #
 #  mass      x      y        z       Ixx   Iyy   Izz    Ixy  Ixz  Iyz
-#
-   27.7949   2.723  1.9054   1.27    0.0   0.0   0.0    ! right wing       
-   27.7949   2.723  -1.9054  1.27    0.0   0.0   0.0    ! left wing  
-   95.9102   2.384  0.0      0.84    0.0   0.0   0.0    ! fuselage
-   17.0451   6.839  0.0      0.3     0.0   0.0   0.0    ! tail
-   
+# 
+""",file_contents,"""
 
 # Created by Jan Vonken, 11-06-2023"""
 
-df = pd.read_csv("AVL_mass.csv")
-print(df)
 
 avl_file = """Wings For Aid
 
@@ -75,7 +75,7 @@ avl_file = """Wings For Aid
 SURFACE
 wing
 #Horseshoe Vortex Distribution
-""",10,  1.0,  20,  1.0,"""   ! Nchord   Cspace   Nspan  Sspace
+""",10, ' ', 1.0, ' ' ,   20, ' ' ,   1.0, ' ' , """   ! Nchord   Cspace   Nspan  Sspace
 #
 # reflect image wing about y=0 plane
 YDUPLICATE
@@ -87,61 +87,61 @@ ANGLE
 
 # x,y,z bias for whole surface
 TRANSLATE
-    """,aircraft.X_LEMAC,     0.00000,     0.00000, """
+    """,aircraft.X_LEMAC, ' ' ,      0.00000, ' ' ,      0.00000, """
 
 # Here the section starts
 #---------------Root Section-------------------------------------------
 #    Xle         Yle         Zle         chord       angle   
 SECTION
-    """,0.0,          0.0,         0.0,         aircraft.rootchord,       0.000, """ 
+    """,0.0, ' ' ,           0.0, ' ' ,          0.0, ' ' ,          aircraft.rootchord, ' ' ,        0.000, """ 
 
 NACA
 4415
 
 CONTROL
 #Cname   Cgain  Xhinge  HingeVec       SgnDup
-flap     """,1.0,    aircraft.xc_aft_spar,    0.0, 1.0, 0.0,    1.0,"""
+flap     """,1.0, ' ' ,     aircraft.xc_aft_spar, ' ' ,     0.0, ' ' ,  1.0, ' ' ,  0.0, ' ' ,     1.0,"""
 
 #---------------End Flap Section-------------------------------------------
 #    Xle         Yle         Zle         chord       angle   
 SECTION
-    """,np.tan(aircraft.AE_sweep_LE)*aircraft.yend_flap,      aircraft.yend_flap,      0.0,        2*aircraft.Sw/(1+aircraft.taper)/aircraft.b*(1-(1-aircraft.taper)/aircraft.b*abs(2*aircraft.yend_flap)),    -aircraft.yend_flap/(aircraft.b/2) * aircraft.wing_twist,"""   
+    """,np.tan(aircraft.AE_sweep_LE)*aircraft.yend_flap, ' ' ,       aircraft.yend_flap, ' ' ,       0.0, ' ' ,         2*aircraft.Sw/(1+aircraft.taper)/aircraft.b*(1-(1-aircraft.taper)/aircraft.b*abs(2*aircraft.yend_flap)), ' ' ,     -aircraft.yend_flap/(aircraft.b/2) * aircraft.wing_twist,"""   
 
 NACA
 4415
 
 CONTROL
 #Cname   Cgain  Xhinge  HingeVec       SgnDup
-flap     """,1.0,    aircraft.xc_aft_spar,    0.0, 1.0, 0.0,    1.0,"""
+flap     """,1.0, ' ' ,     aircraft.xc_aft_spar, ' ' ,     0.0, ' ' ,  1.0, ' ' ,  0.0, ' ' ,     1.0,"""
 
 
 #---------------Begin Aileron Section-------------------------------------------
 #    Xle         Yle         Zle         chord       angle   
 SECTION
-    """,np.tan(aircraft.AE_sweep_LE)*aircraft.ystart_ail,      aircraft.ystart_ail,       0.0,         2*aircraft.Sw/(1+aircraft.taper)/aircraft.b*(1-(1-aircraft.taper)/aircraft.b*abs(2*aircraft.ystart_ail)),      -aircraft.ystart_ail/(aircraft.b/2) * aircraft.wing_twist,"""   
+    """,np.tan(aircraft.AE_sweep_LE)*aircraft.ystart_ail, ' ' ,       aircraft.ystart_ail, ' ' ,        0.0, ' ' ,          2*aircraft.Sw/(1+aircraft.taper)/aircraft.b*(1-(1-aircraft.taper)/aircraft.b*abs(2*aircraft.ystart_ail)), ' ' ,       -aircraft.ystart_ail/(aircraft.b/2) * aircraft.wing_twist,"""   
 
 NACA
 4415
 
 CONTROL
 #Cname   Cgain  Xhinge  HingeVec       SgnDup
-aileron  """,1.0,    aircraft.xc_aft_spar,    0.0, 1.0, 0.0,   -1.0,"""
+aileron  """,1.0, ' ' ,     aircraft.xc_aft_spar, ' ' ,     0.0, ' ' ,  1.0, ' ' ,  0.0, ' ' ,    -1.0,"""
 
 #---------------End Aileron Section-------------------------------------------
 #    Xle         Yle         Zle         chord       angle   
 SECTION
-    """,np.tan(aircraft.AE_sweep_LE)*aircraft.yend_ail,      aircraft.yend_ail,       0.0,         2*aircraft.Sw/(1+aircraft.taper)/aircraft.b*(1-(1-aircraft.taper)/aircraft.b*abs(2*aircraft.yend_ail)),      -aircraft.yend_ail/(aircraft.b/2) * aircraft.wing_twist,"""   
+    """,np.tan(aircraft.AE_sweep_LE)*aircraft.yend_ail, ' ' ,       aircraft.yend_ail, ' ' ,        0.0, ' ' ,          2*aircraft.Sw/(1+aircraft.taper)/aircraft.b*(1-(1-aircraft.taper)/aircraft.b*abs(2*aircraft.yend_ail)), ' ' ,       -aircraft.yend_ail/(aircraft.b/2) * aircraft.wing_twist,"""   
 
 NACA
 4415
 
 CONTROL
 #Cname   Cgain  Xhinge  HingeVec       SgnDup
-aileron  """,1.0,    aircraft.xc_aft_spar,    0.0, 1.0, 0.0,  -1.0,"""
+aileron  """,1.0, ' ' ,     aircraft.xc_aft_spar, ' ' ,     0.0, ' ' ,  1.0, ' ' ,  0.0, ' ' ,   -1.0,"""
 
 #-----------Tip Section------------------
 SECTION
-    """,np.tan(aircraft.AE_sweep_LE)*aircraft.b/2,    aircraft.b/2,    0.0,        aircraft.tipchord,        -aircraft.wing_twist,"""   
+    """,np.tan(aircraft.AE_sweep_LE)*aircraft.b/2, ' ' ,     aircraft.b/2, ' ' ,     0.0, ' ' ,         aircraft.tipchord, ' ' ,         -aircraft.wing_twist,"""   
 
 NACA
 4415
@@ -151,7 +151,7 @@ NACA
 #
 SURFACE
 H-Stab
-""",8,  1.0,  5,  1.0,"""  !  Nchord   Cspace
+""",8, ' ' ,   1.0, ' ' ,   5, ' ' ,   1.0,"""  !  Nchord   Cspace
 #
 # reflect image wing about y=0 plane
 YDUPLICATE
@@ -163,38 +163,38 @@ ANGLE
 
 # x,y,z bias for whole surface
 TRANSLATE
-   """,aircraft.X_LEMAC + 0.25 * aircraft.rootchord + aircraft.l_h - 0.25*aircraft.AE_rootchord_h,     0.0,     0.0,"""
+   """,aircraft.X_LEMAC + 0.25 * aircraft.rootchord + aircraft.l_h - 0.25*aircraft.AE_rootchord_h, ' ' ,      0.0, ' ' ,      0.0,"""
 
 #-----------------------Root Section-----------------------------------
 #    Xle         Yle         Zle         chord       angle 
 SECTION
-   """,0.0,          0.0,        0.0,         aircraft.AE_rootchord_h,      0.000,"""   
+   """,0.0, ' ' ,           0.0, ' ' ,         0.0, ' ' ,          aircraft.AE_rootchord_h, ' ' ,       0.000,"""   
 
 NACA
 0012
 
 CONTROL
 #Cname   Cgain  Xhinge  HingeVec       SgnDup
-elevator  """,1.0,    0.001,    0.0, 1.0, 0.0,    1.0,"""
+elevator  """,1.0, ' ' ,     0.001, ' ' ,     0.0, ' ' ,  1.0, ' ' ,  0.0, ' ' ,     1.0,"""
 
 #-------------------Tip Section-------------------------
 #    Xle         Yle         Zle         chord       angle 
 SECTION
-     """,0.0,         aircraft.AE_b_h/2,       0.0,       aircraft.AE_tipchord_h,     0.000,"""  
+     """,0.0, ' ' ,          aircraft.AE_b_h/2, ' ' ,        0.0, ' ' ,        aircraft.AE_tipchord_h, ' ' ,      0.000,"""  
 
 NACA
 0012
 
 CONTROL
 #Cname   Cgain  Xhinge  HingeVec       SgnDup
-elevator  """,1.0,    0.001,    0.0, 1.0, 0.0,    1.0,"""
+elevator  """,1.0, ' ' ,     0.001, ' ' ,     0.0, ' ' ,  1.0, ' ' ,  0.0, ' ' ,     1.0,"""
 
 #
 #==============================================================
 #
 SURFACE
 V-Stab
-""",8,  1.0,  10,  aircraft.xc_aft_spar,"""  ! Nchord   Cspace
+""",8, ' ' ,   1.0, ' ' ,   10, ' ' ,   aircraft.xc_aft_spar,"""  ! Nchord   Cspace
 
 # twist angle bias for whole surface
 ANGLE
@@ -202,42 +202,50 @@ ANGLE
 
 # x,y,z bias for whole surface
 TRANSLATE
-   """,aircraft.X_LEMAC + 0.25 * aircraft.rootchord + aircraft.l_h - 0.25*aircraft.AE_rootchord_h,     0.00000,     0.00000,"""
+   """,aircraft.X_LEMAC + 0.25 * aircraft.rootchord + aircraft.l_h - 0.25*aircraft.AE_rootchord_h, ' ' ,      0.00000, ' ' ,      0.00000,"""
 #-------------------Lower Section-------------------------------------------
 #    Xle         Yle         Zle         chord       angle   
 SECTION
-   """,0.0,           0.0,         0.0,        aircraft.AE_rootchord_v,       0.000,"""   
+   """,0.0, ' ' ,            0.0, ' ' ,          0.0, ' ' ,         aircraft.AE_rootchord_v, ' ' ,        0.000,"""   
 
 NACA 
 0012
 
 #CONTROL
-#rudder    """,1.0,    0.40,   0.0, 0.0, 1.0,"""
+#rudder    """,1.0, ' ' ,     0.40, ' ' ,    0.0, ' ' ,  0.0, ' ' ,  1.0,"""
 #-----------------Upper Section----------------------
 SECTION
-   """,np.tan(aircraft.AE_lambda_LE_v) * aircraft.b_v,          0.0,        aircraft.b_v,        aircraft.AE_tipchord_v,      0.000,"""  
+   """,np.tan(aircraft.AE_lambda_LE_v) * aircraft.b_v, ' ' ,           0.0, ' ' ,         aircraft.b_v, ' ' ,         aircraft.AE_tipchord_v, ' ' ,       0.000,"""  
 
 NACA
 0012
 
 #CONTROL
-#rudder    """,1.0,    0.40,    0.0, 0.0, 1.0,"""
+#rudder    """,1.0, ' ' ,     0.40, ' ' ,     0.0, ' ' ,  0.0, ' ' ,  1.0,"""
 #==============================================================
 
 #Created by Jan Vonken 10-06-2023"""
+
+f.close()
+
 string = str()
 for i in range(len(avl_file)):
     string = string + str(avl_file[i])
 
-print(string)
+#print(string)
 
-path = r"C:\Users\jarno\OneDrive\Documenten\WingsForAid\aerodynamics\WFA.avl"
-
-#r"C:\Users\jarno\OneDrive\Documenten\WingsForAid\aerodynamics\WFA.avl"
-""
+path = r"/Users/janvonken/Desktop/TU/3rd Year/DSE/simulating/Avl/OwnCraft/WingsForAid2.avl"""
 f = open(path, "w")
-
 f.write(string)
+f.close()
 
+string = str()
+for i in range(len(avl_mass)):
+    string = string + str(avl_mass[i])
 
+#print(string)
 
+path = r"/Users/janvonken/Desktop/TU/3rd Year/DSE/simulating/Avl/OwnCraft/WingsForAid2.mass"""
+f = open(path, "w")
+f.write(string)
+f.close()
