8556f191181bf0ca253e5b696c51144c7b4e7a9d
Jarno11 <jarno.platenburg@gmail.com>
2023-06-12 17:16:44 +0200

    main aero

--- aerodynamics/main_aero.py
+++ aerodynamics/main_aero.py
@@ -3,15 +3,12 @@ import os.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from parameters import UAV
 
-
 import wing_planform as wp
 import horizontal_tail_design as htd
+import vertical_tail_design as vtd
 
 aircraft = UAV('aircraft')
 
 wp.main_wing_planform(aircraft)
 htd.horizontal_tail_planform(aircraft)
-
-
-
-
+vtd.horizontal_tail_planform(aircraft)
