4a2469a6204f869af8eeb80170dfaedb3be36903
jwiacek <jwiacek@student.tudelft.nl>
2023-06-16 17:34:43 +0200

    ansys some verification

--- structures/ansys_convergence_study.py
+++ structures/ansys_convergence_study.py
@@ -13,3 +13,25 @@ plt.xlim(max(mesh), min(mesh))
 plt.xlabel("Mesh element size [mm]",size=15)
 plt.ylabel("Nondimentionalised node deformation [-]",size=15)
 plt.show()
+
+load = np.array([10,50,100,500,1000,5000,10000,50000])
+E = 2*10**11
+I = 7.6694*10**(-7)
+L=1
+
+anal_deformation =1000*load*L/(3*E*I)
+anal_max_moment = load*L
+#print(anal_deformation)
+ansys_deformation = np.array([0.02307,0.11536,0.23072,1.1536,2.3072,11.536,23.072,115.36])
+ansys_max_moment = 0.001*np.array([9750,48750,97500,4.875E5,9.75E5,4.875E6,9.75E6,4.875E7])
+
+plt.plot(load,anal_deformation,"o-", color = "orange")
+plt.plot(load,ansys_deformation,"o-",color="blue")
+plt.grid()
+plt.yscale("log")
+plt.xscale("log")
+#plt.xlim(max(load), min(load))
+#plt.ylim(max())
+plt.xlabel("Load [N]",size=15)
+plt.ylabel("Absolute deformation [mm]",size=15)
+plt.show()
