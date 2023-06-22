f35ebf303de9f20d28088e2d0e29be7c2eabe41b
Timdnb <timdnblanken@gmail.com>
2023-06-22 09:14:29 +0200

    add drag estimation

--- aerodynamics/main_aero.py
+++ aerodynamics/main_aero.py
@@ -5,9 +5,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.
 import aerodynamics.wing_planform as wp
 import aerodynamics.horizontal_tail_design as htd
 import aerodynamics.vertical_tail_design as vtd
+import aerodynamics.drag_estimation as de
 
 def run_aero(aircraft):
     wp.main_wing_planform(aircraft)
     htd.horizontal_tail_planform(aircraft)
     vtd.horizontal_tail_planform(aircraft)
+    de.zero_liftdrag(aircraft)
 
